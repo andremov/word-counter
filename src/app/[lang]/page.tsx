c54fa851928e5be3cf56bde7fe010b4cfa1310c5
Andres-XL <amovillaw01@gmail.com>
2024-12-03 17:04:45 -0500

    revise page props for next 15

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-
 import WordCounter from "~/_components/word-counter";
 import LocaleSwitcher from "~/_components/locale-switcher";
 
 type PageProps = {
-  params: { lang: "en" | "es" };
+  params: Promise<{ lang: "en" | "es" }>;
 };
 
-export default function HomePage({ params }: PageProps) {
+export default async function HomePage({ params }: PageProps) {
+  const lang = (await params).lang;
+
   return (
     <main className="h-screen max-h-screen pb-4 pt-16">
-      <LocaleSwitcher lang={params.lang} />
-      <WordCounter lang={params.lang} />
+      <LocaleSwitcher lang={lang} />
+      <WordCounter lang={lang} />
     </main>
   );
 }
