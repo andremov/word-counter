feb37d1d869f0d82f16a76dbc1389a62ce1a7604
Andres-XL <amovillaw01@gmail.com>
2025-07-17 17:56:46 -0500

    replace generic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import WordCounter from "~/_components/word-counter";
-import LocaleSwitcher from "~/_components/locale-switcher";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <main className="h-screen max-h-screen pb-4 pt-16">
-      <LocaleSwitcher />
-      <WordCounter />
-    </main>
-  );
+export default function RootPage() {
+  redirect("/en");
 }
